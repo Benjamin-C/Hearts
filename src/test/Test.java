97a715e8f646cc0ff87e89fc7ed7dba2c5cf423e
Benjamin-C <bencrall@gmail.com>
2018-01-20 19:01:09 -0800

    Trying to prevent large log files from being uploaded to GitHub

--- src/test/Test.java
+++ src/test/Test.java
@@ -6,7 +6,7 @@ import game.GameHandler;
 
 public class Test {
 	public static void main(String args[])  {
-		int repeat = 64;
+		int repeat = 2; // Keep less than
 		ArrayList<Integer> score = new ArrayList<Integer>();
 		ArrayList<Integer> hands = new ArrayList<Integer>();
 		Output out = new Output();
@@ -20,20 +20,20 @@ public class Test {
 			hands.add(g.getHands());
 		}
 		long time = System.currentTimeMillis() - start;
-		
+		out.setNum(-1);
 		out.println(score);
 		out.println(hands);
-		double tot = 0;
+		double tot = 0.0;
 		int hds = 0;
 		for(int i = 0; i < score.size(); i++) {
 			tot = tot + score.get(i);
 			hds = hds + hands.get(i);
 		}
-		out.println("tot " + tot + " | hds " + hds);
+		out.println("tot " + tot + " | hds " + hds + " | tim " + time);
 		tot = tot / (double) hds;
 		out.println("Average hand score: " + tot);
 		out.println("Time: " + time);
-		double hps = (double) hds / ((double) time * 1000.0);
+		double hps = (double) hds / ((double) time / 1000.0);
 		out.println("hps: " + hps); 
 	}
 }
